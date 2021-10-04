d6f59e57f69a5c27d78cdf910027dca0b84b76d4
|M| <manicprone@gmail.com>
2021-10-04 07:07:53 -0600

    Removed chain_id variable

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
-  cluster_name        = "chain-services-${var.chain_id}-${var.chain_network_name}"
-  api_service_name    = "${var.chain_id}-${var.chain_network_name}-indexer-api"
-  worker_service_name = "${var.chain_id}-${var.chain_network_name}-indexer-worker"
-  api_task_name       = "${var.api_task_name}-${var.chain_id}-${var.chain_network_name}"
-  worker_task_name    = "${var.worker_task_name}-${var.chain_id}-${var.chain_network_name}"
+  cluster_name        = "chain-services-${var.chain_network_name}"
+  api_service_name    = "${var.chain_network_name}-indexer-api"
+  worker_service_name = "${var.chain_network_name}-indexer-worker"
+  api_task_name       = "${var.api_task_name}-${var.chain_network_name}"
+  worker_task_name    = "${var.worker_task_name}-${var.chain_network_name}"
   mongo_uri           = "mongodb://${var.mongo_db_user}:${var.mongo_db_password}@${var.mongo_db_host}:27017/${var.mongo_db_name}?authSource=admin"
   vpc_workspace_name  = "aws-networking-infra-${var.env_alias}"
 }
@@ -12,7 +12,6 @@ locals {
   tags = {
     environment   = var.env_alias
     chain_service = "ethereum-indexer"
-    chain_id      = var.chain_id
     chain_network = var.chain_network_name
   }
 }
