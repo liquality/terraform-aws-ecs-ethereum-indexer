da1dac5869433887b125f46b237d25d287bd69da
|M| <manicprone@gmail.com>
2021-11-23 17:47:04 -0700

    Changing naming to avoid collisions

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   cluster_name        = "chain-indexer-${var.chain_network_name}"
-  api_service_name    = "${var.chain_network_name}-indexer-api"
-  worker_service_name = "${var.chain_network_name}-indexer-worker"
+  api_service_name    = "indexer-api-${var.chain_network_name}"
+  worker_service_name = "indexer-worker-${var.chain_network_name}"
   api_task_name       = "${var.api_task_name}-${var.chain_network_name}"
   worker_task_name    = "${var.worker_task_name}-${var.chain_network_name}"
   mongo_uri           = "mongodb://${var.mongo_db_user}:${var.mongo_db_password}@${var.mongo_db_host}:27017/${var.mongo_db_name}?authSource=admin"
@@ -11,7 +11,7 @@ locals {
 locals {
   tags = {
     environment   = var.env_alias
-    chain_service = "ethereum-indexer"
+    chain_service = "chain-indexer"
     chain_network = var.chain_network_name
   }
 }
